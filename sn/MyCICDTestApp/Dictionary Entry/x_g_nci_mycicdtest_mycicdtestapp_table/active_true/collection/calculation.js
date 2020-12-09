b6158cd58e28edfebc8bd0cb5b0b5abc8b8d99f7
Rizk <rizknt@nih.gov>
2020-12-08 19:17:58 -0500

    Changes by 'undefined' on 'MyCICDTestApp - 1.0.0' -- Build #1

---
+++ sn/MyCICDTestApp/Dictionary Entry/x_g_nci_mycicdtest_mycicdtestapp_table/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : MyCICDTestApp
+ * ClassName   : sys_dictionary
+ * Created On  : 2020-11-02 19:28:29
+ * Created By  : rizknt@nih.gov
+ * Updated On  : 2020-11-03 11:43:32
+ * Updated By  : system
+ * URL         : /sys_dictionary.do?sys_id=0cc3abe81b18641094edddb6bc4bcbc0
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
